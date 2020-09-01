773476139ccc74d843b3c10c24093408a6931737
wuff1996 <1027309561@qq.com>
2020-09-01 09:01:33 +0800

    匿名函数

--- cal/cmd/GetKestNumber/main.go
+++ cal/cmd/GetKestNumber/main.go
@@ -2,39 +2,39 @@ package main
 
 import "fmt"
 
-const k = 15
-
-var slice = make([]int, 0)
+const k = 2
 
 func main() {
-
-	fmt.Println(GetKestNumber(1))
-	fmt.Println(GetKestNumber(2))
-	fmt.Println(GetKestNumber(0))
-	fmt.Println(GetKestNumber(5))
-	fmt.Println(GetKestNumber(6))
-	fmt.Println(GetKestNumber(0))
-	fmt.Println(GetKestNumber(1))
-	fmt.Println(GetKestNumber(2))
-	fmt.Println(GetKestNumber(0))
-	fmt.Println(slice)
+	getK := GetKestNumber()
+	fmt.Println(getK(1))
+	fmt.Println(getK(2))
+	fmt.Println(getK(0))
+	fmt.Println(getK(5))
+	fmt.Println(getK(6))
+	fmt.Println(getK(0))
+	fmt.Println(getK(1))
+	fmt.Println(getK(2))
+	fmt.Println(getK(0))
 
 }
 
-func GetKestNumber(n int) int {
-	if len(slice) < k+1 {
-		slice = append(slice, n)
-	} else {
-		slice[k] = n
-	}
-	for i := len(slice) - 1; i > 0; i-- {
-		if slice[i] > slice[i-1] {
-			slice[i], slice[i-1] = slice[i-1], slice[i]
+func GetKestNumber() func(n int) int {
+	var slice = make([]int, 0)
+	return func(n int) int {
+		if len(slice) < k+1 {
+			slice = append(slice, n)
+		} else {
+			slice[k] = n
 		}
+		for i := len(slice) - 1; i > 0; i-- {
+			if slice[i] > slice[i-1] {
+				slice[i], slice[i-1] = slice[i-1], slice[i]
+			}
+		}
+		if len(slice) < k {
+			return slice[len(slice)-1]
+		}
+		return slice[k-1]
 	}
-	if len(slice) < k {
-		return slice[len(slice)-1]
-	}
-	return slice[k-1]
-}
 
+}
